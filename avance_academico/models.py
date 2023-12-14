89d4374e6f7c73ba31104bad1869fc9aba4abcdb
augusto <augustocastro@gmail.com>
2023-12-14 14:07:53 -0300

    commit augusto

--- avance_academico/models.py
+++ avance_academico/models.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Materia(models.Model):
+    Tipo_cursada = (
+        ('ANUAL', 'ANUAL'),
+        ('PRIMER CUATRIMESTRE', 'PRIMER CUATRIMESTRE'),
+        ('SEGUNDO CUATRIMESTRE', 'SEGUNDO CUATRIMESTRE')
+    )
     # Campos básicos
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=100)
+    anio = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    duracion = models.CharField(max_length=50, choices=Tipo_cursada, null=True, blank=True)
+    # Relaciones
+    correlativas = models.ManyToManyField('self', symmetrical=False, blank=True)
 
     # Relaciones
     correlativas = models.ManyToManyField('self', symmetrical=False, blank=True)
@@ -15,10 +27,13 @@ class Materia(models.Model):
 class Estudiante(models.Model):
     # Campos básicos
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dni = models.IntegerField(default=0)
+    nombre = models.CharField(max_length=200,default="0")
+    apellido = models.CharField(max_length=200,default="0")
 
     # Relaciones
     materias_aprobadas = models.ManyToManyField('Materia', related_name='estudiantes_aprobados', blank=True)
     materias_en_curso = models.ManyToManyField('Materia', related_name='estudiantes_en_curso', blank=True)
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido} ({self.matricula})"
+        return f"{self.nombre} {self.apellido} ({self.dni})"
--- avance_academico/views.py
+++ avance_academico/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views import generic
+from .models import Materia
+
+"""def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+"""
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Materia.objects.order_by("anio")[:50]
