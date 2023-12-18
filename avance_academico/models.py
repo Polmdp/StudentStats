f5fabaaafbeceb1c4317335848cfb988db34474c
Pablo V <pablo.valdesogo@gmail.com>
2023-12-18 08:45:34 -0300

    initial

--- avance_academico/models.py
+++ avance_academico/models.py
@@ -42,6 +42,6 @@ class Profesor(models.Model):
     #campos basicos
     nombre_profesor = models.CharField(max_length=200, default="0")
     apellido_profesor = models.CharField(max_length=200, default="0")
-    materias_dadas= models.ManyToManyField('self', symmetrical=False, blank=True)
+    materias = models.ManyToManyField('Materia', symmetrical=False, blank=True)
     def __str__(self):
         return f"{self.nombre_profesor}{self.apellido_profesor}"
--- avance_academico/urls.py
+++ avance_academico/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 app_name = "avance_academico"
 urlpatterns = [
-path("", views.IndexView.as_view(), name="index"),
- path("\n", views.ListaMaterias.as_view(), name="lista-materias"),
- path("specifics/<int:id>/",views.detallemateria,name="detalle_materia")
-
-
+    path("", views.IndexView.as_view(), name="index"),
+    path("lista_materias/", views.ListaMaterias.as_view(), name="lista-materias"),
 ]
