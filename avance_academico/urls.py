da156e39908e2fa6071bbde0cdbbc0cf4b262a1b
augusto <augustocastro@gmail.com>
2023-12-19 09:33:29 -0300

    commit augusto

--- avance_academico/urls.py
+++ avance_academico/urls.py
@@ -6,4 +6,7 @@ app_name = "avance_academico"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("lista_materias/", views.ListaMaterias.as_view(), name="lista-materias"),
+    path("lista_profesores/",views.ListaProfesores.as_view(),name="lista-profesores"),
+    path("detail/<int:id>/",views.detallemateria,name="detail-materia"),
+    path("detail_profesor/<int:id>/",views.detalleprofesor,name="detail-profesor")
 ]
--- avance_academico/views.py
+++ avance_academico/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 from django.views import generic
-from .models import Materia
+from .models import Materia,Profesor
 
 
 class IndexView(generic.ListView):
@@ -24,9 +24,15 @@ class ListaMaterias(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Materia.objects.order_by("anio")[:50]
-
+class ListaProfesores(generic.ListView):
+    template_name="avance_academico/profesor_list.html"
+    context_object_name = "profesor_list"
+    def get_queryset(self):
+        return Profesor.objects.order_by("nombre_profesor")[:10]
 def detallemateria(request,id):
     materia = get_object_or_404(Materia, pk=id)
     return render(request, "avance_academico/detail_materia.html", {"materia": materia})
-
+def detalleprofesor(request,id):
+    profesor=get_object_or_404(Profesor,pk=id)
+    return render(request,"avance_academico/detail_profesor.html",{"profesor":profesor})
 
